e0806f55506c4e321372875f86ed6d8644d11b06
Stefan Stoykov <stoykovnet@gmail.com>
2016-03-02 14:42:51 +0100

    Authentication factory is created on the front-end.

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -3,7 +3,7 @@ var app = angular.module('ums', ['ui.router']);
 app.config([
     '$stateProvider',
     '$urlRouterProvider',
-    function($stateProvider, $urlRouterProvider) {
+    function ($stateProvider, $urlRouterProvider) {
 
         $stateProvider
             .state('home', {
@@ -65,4 +65,78 @@ app.controller('MainCtrl', [
             $scope.lastName = '';
         }
 
+    }]);
+
+/**
+ * Use auth to maintain a session for user.
+ */
+app.factory('auth', ['$http', '$window',
+    function ($http, $window) {
+        var auth = {};
+
+        auth.saveToken = function (token) {
+            $window.localStorage['ums-user-token'] = token;
+        };
+
+        auth.getToken = function () {
+            return $window.localStorage['ums-user-token'];
+        }
+
+        /*
+         * Check if current user has a valid token.
+         */
+        auth.isLoggedIn = function () {
+            var token = auth.getToken();
+
+            if (token) {
+                var payload = JSON.parse($window.atob(token.split('.')[1]));
+
+                return payload.expire > Date.now() / 1000;
+            } else {
+                return false;
+            }
+        };
+
+        /*
+         * Get user's email if he or she is logged in.
+         */
+        auth.currentUser = function () {
+            if (auth.isLoggedIn()) {
+                var token = auth.getToken();
+                var payload = JSON.parse($window.atob(token.split('.')[1]));
+
+                return payload.email;
+            }
+        };
+
+        /*
+         * Send user data to server to register him or her.
+         * On success a token will be saved, otherwise user should retry registration.
+         */
+        auth.register = function (user) {
+            return $http.post('/authentication/post', user)
+                .success(function (data) {
+                    auth.saveToken(data.token);
+                });
+        };
+
+        /*
+         * Send user credentials to server to log him or her in.
+         * On success a token will be saved, otherwise user should retry logging in.
+         */
+        auth.logIn = function (user) {
+            return $http.post('/login', user)
+                .success(function (data) {
+                    auth.saveToken(data.token);
+                });
+        };
+
+        /*
+         * Remove user token to log him or her out.
+         */
+        auth.logOut = function () {
+            $window.localStorage.removeItem('ums-user-token');
+        };
+
+        return auth;
     }]);
